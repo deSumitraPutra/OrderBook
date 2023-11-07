5d474841d47ae1dbc15ccd5ffcf9b473f314e4d8
deSumitraPutra <dewa.putra@btpn.com>
2023-11-07 19:51:34 +0700

    add PriceLevelRow component

---
+++ src/components/OrderBook/PriceLevelRow/PriceLevelRow.test.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable testing-library/no-node-access */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import PriceLevelRow from './PriceLevelRow.component';
+
+test('renders price level row properly - not reversed', () => {
+  render(<PriceLevelRow
+    price={'1000'}
+    reversedFieldsOrder={false}
+    size={'500'}
+    total={'50000'} windowWidth={800} />);
+  const priceElement = screen.getByTestId('price-level-row');
+  const spanElements = priceElement.querySelectorAll('span');
+  expect(spanElements[2]).toHaveClass('price');
+});
+
+
+test('renders price level row properly - reversed', () => {
+  render(<PriceLevelRow
+    price={'1000'}
+    reversedFieldsOrder={true}
+    size={'500'}
+    total={'50000'} windowWidth={800} />);
+  const priceElement = screen.getByTestId('price-level-row');
+  const spanElements = priceElement.querySelectorAll('span');
+  expect(spanElements[0]).toHaveClass('price');
+});
